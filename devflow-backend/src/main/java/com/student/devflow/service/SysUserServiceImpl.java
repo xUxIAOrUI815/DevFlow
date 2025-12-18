1c3d648b5ae051b2618e5fecb76e5d08c579760d
XuXiaoRui <1531793838@qq.com>
2025-12-18 21:32:31 +0800

    添加注册用户的实现

--- devflow-backend/src/main/java/com/student/devflow/service/SysUserServiceImpl.java
+++ devflow-backend/src/main/java/com/student/devflow/service/SysUserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.student.devflow.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.student.devflow.entity.SysUser;
 import com.student.devflow.mapper.SysUserMapper;
@@ -8,4 +9,28 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
+
+    @Override
+    public void register(SysUser user){
+        // 校验非空
+        if(user.getUsername() == null || user.getPassword () == null){
+            throw new RuntimeException("用户名和密码不能为空");
+        }
+
+        // 检查用户名是否已经存在
+        QueryWrapper<SysUser> query = new QueryWrapper<>();
+        query.eq("username",user.getUsername());
+        if(this.count(query)>0){
+            throw new RuntimeException("用户名已存在，请换一个");
+        }
+
+        // 设置默认值
+        if(user.getRealName() == null){
+            user.setRealName(user.getUsername());   // 默认使用用户名
+        }
+        user.setAvatar("https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png");
+
+        // 真实项目需要给密码加密
+        this.save(user);
+    }
 }
